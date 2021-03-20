9554ba7c36065aabebd77f28ebd4ea50202d78c6
Krishkap7 <30948697+Krishkap7@users.noreply.github.com>
2021-03-20 14:50:11 -0700

    Create model.py

---
+++ model.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+"""Import Necesarry Libraries"""
+
+# Commented out IPython magic to ensure Python compatibility.
+try:
+#     %tensorflow_version 2.x
+#     %load_ext tensorboard   
+except Exception:
+  pass
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorboard.plugins.hparams import api as hp
+
+from sklearn.preprocessing import LabelBinarizer, LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix
+import imgaug.augmenters as iaa
+import matplotlib.pyplot as plt
+from imutils import paths
+import seaborn as sns
+import imgaug as aug
+import pandas as pd
+import numpy as np
+import datetime
+import h5py
+import cv2
+import os
