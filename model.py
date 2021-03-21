c4fa0a00a8f50aea1a9d880c092c23b92e025668
saphiashi <66221568+saphiashi@users.noreply.github.com>
2021-03-20 20:32:45 -0700

    Update model.py

--- model.py
+++ model.py
@@ -1,6 +1,4 @@
-# -*- coding: utf-8 -*-
-
-"""Import Necesarry Libraries"""
+"""Import libraries"""
 
 try:
 #     %tensorflow_version 2.x
@@ -27,11 +25,11 @@ import h5py
 import cv2
 import os
 
-# helper functions
+# functions
 def display_image(img):
-    """
-    function to show a picture of a chest x-ray scan
-    """
+    '''
+    function to show a picture 
+    '''
     if type(img) == 'str':
         img = plt.imread(img)
     plt.imshow(img, cmap = 'gray')
@@ -39,79 +37,87 @@ def display_image(img):
     plt.grid(False)
     plt.axis('off')
     plt.show()
- """Dataset download"""
+
+"""Dataset download"""
 
 !git clone https://github.com/Krishkap7/DV-Hacks---Covid-19-Diagnostic-Device
 
-"""Scan Normal vs. Covid images"""
+"""Scan Healthy vs. Covid images and 1-hot encode labels
 
-data_path = 'DV-Hacks---Covid-19-Diagnostic-Device/real_data'
+"""
 
-# read all the files in the path
+datasetPath = 'DV-Hacks---Covid-19-Diagnostic-Device/real_data'
+
+# read all files from path
 print("loading images...")
-imagePaths = list(paths.list_images(data_path))
+imagePaths = list(paths.list_images(datasetPath))
 
 data = []
 
-i = 0 #initialize the counters and add all 3 in the model 
-for imagePath in imagePaths:
-    # extract the class label from the filename
-    label = imagePath.split(os.path.sep)[-2]
+# select no. of classes to be loaded for Healthy, Bacterial Pneumonia, and Viral Pneumonia patients 
+numHealthyImages = 364
 
-    if label == 'normal' and i < num_normal_images:
-      data.append((imagePath,label))
+i = 0 # initialize all 3 counters to decr. the no. of images loaded 
+for imagePath in imagePaths:
+    # extract class label from file name
+    imageLabel = imagePath.split(os.path.sep)[-2]
+
+    if imageLabel == 'healthy' and i < numHealthyImages:
+      data.append((imagePath,imageLabel))
       i += 1
 
-    elif label == 'covid':
-      data.append((imagePath,label))
-print('\n Displaying a dataframe...')
+    elif imageLabel == 'covid':
+      data.append((imagePath,imageLabel))
 
-# Create a pandas dataframe
+print('\n creating a DataFrame...')
+# retrieve pandas dataframe from our data in the list 
 data = pd.DataFrame(data, columns=['image', 'label'],index=None)
 
-# Shuffle the data 
+# randomize data
 data = data.sample(frac=1.).reset_index(drop=True)
 print(data.head(10))
 
-current_image = plt.imread(data["image"][0])
-display_image(current_image)
-del current_image
+tmp_img = plt.imread(data["image"][0])
+display_image(tmp_img)
+del tmp_img
 
-# perform one-hot encoding on the labels
-print('\n creating a one-hot encoding')
-print('\tExample to display:', data["label"][0])
+# do 1-hot encoding on labels
+print('\n one-hot encoding...')
+print('\tExample scan label:', data["label"][0])
 
 data["label"] = LabelEncoder().fit_transform(data["label"])
 print('\tOne-hot encoded label: ',data["label"][0])
-print('\n re creating the dataframe')
+print('\n updating data DataFrame...')
 print(data.head(10))
 
+"""Visualize example X-ray scans
 
-"""Visualise example X-ray scans"""
+"""
 
-# select the number of examples to display - make sure that the covid and normal ones are the same number
-num_examples = 5
-normal_example = (data[data['label']==0]['image'].iloc[30:30+num_examples]).tolist()
-covid_example = (data[data['label']==1]['image'].iloc[5:5+num_examples]).tolist()
+# choose the no. of examples to show in order to confirm that the covid and healthy ones are same no.
+numExamples = 5
+healthyExample = (data[data['label']==0]['image'].iloc[30:30+numExamples]).tolist()
+covidExample = (data[data['label']==1]['image'].iloc[5:5+numExamples]).tolist()
 
-samples = covid_example + normal_example
-del covid_example, normal_example
+samples = covidExample + healthyExample
+del covidExample, healthyExample
 
 color = 'black'
-# Plot the data 
-f, ax = plt.subplots(2, num_examples, figsize=(40,15))
-for i in range(num_examples * 2):
+
+# plot data 
+f, ax = plt.subplots(2, numExamples, figsize=(40,15))
+for i in range(numExamples * 2):
     img = cv2.imread(samples[i])
     img = cv2.resize(img, (224, 224))
-    if i < num_examples:
-        ax[0, i % num_examples].imshow(img, cmap='gray')
+    if i < numExamples:
+        ax[0, i % numExamples].imshow(img, cmap='gray')
         ax[0, i % sample_num].set_title("Covid-19", fontsize = 30, color = color)
     else:
-        ax[1, i % num_examples].imshow(img, cmap='gray')
-        ax[1, i % num_examples].set_title("Normal", fontsize = 30, color = color)
+        ax[1, i % numExamples].imshow(img, cmap='gray')
+        ax[1, i % numExamples].set_title("Healthy", fontsize = 30, color = color)
 
-    ax[i // num_examples, i%num_examples].axis('off')
-    ax[i // num_examples, i % num_examples].set_aspect('auto')
+    ax[i // numExamples, i%numExamples].axis('off')
+    ax[i // numExamples, i % numExamples].set_aspect('auto')
 
 plt.tight_layout()
 plt.show()
